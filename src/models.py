af864a0007e3f3391df19fa6bb1ffcf36b3f1bbf
PaolaC <paocastrob@gmail.com>
2019-08-08 17:40:42 +0000

    adding tables

--- src/models.py
+++ src/models.py
@@ -13,7 +13,31 @@ class Person(Base):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(255), nullable=False)
+
+class Likes(Base):
+    __tablename__ = 'Likes'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comments_id = Column(Integer, ForeignKey('comments.id'))
+    name = Column(String(255), nullable=False)
+
+class Comments(Base):
+    __tablename__ = 'Comments'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    name = Column(String(255), nullable=False)
+
+class Alien(Base):
+    __tablename__ = 'alien'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    name = Column(String(255), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
